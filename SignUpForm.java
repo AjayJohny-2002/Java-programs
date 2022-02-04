db65bf5785897c241344bfb88466a94c9657ae6f
Ajay Johny <83871660+AjayJohny-2002@users.noreply.github.com>
2022-02-04 12:53:45 +0530

    Update SignUpForm.java

--- SignUpForm.java
+++ SignUpForm.java
@@ -1,7 +1,7 @@
 /**************
  * Author   : Ajay Johny
  * Program  : Simple signUp Form
- ***************/
+ ****************/
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,37 +12,48 @@ public class SignUpForm extends Frame implements  ActionListener{
     TextField Name = new TextField(20);
     TextField MailId = new TextField(20);
     TextField Password = new TextField(20);
+    TextField mobileNo = new TextField(20);
     Button button;
-    Label label1,label2,label3;
+    Label label1,label2,label3,label4;
 
     public SignUpForm(){
         
+        //Title
         setTitle(" Sign Up Form ");
         setLayout(new FlowLayout());
         setVisible(true);
-        setSize(500,400);
+        setSize(200,400);
 
         button = new Button("submit");
-
+        // for getting name
         label1 = new Label("Name");
-        label1.setAlignment((int) LEFT_ALIGNMENT);
+        label1.setAlignment(Label.LEFT);
         add(label1);
         add(Name);
-        Name.getText();
-
+        
+        
+        // for mail id field
         label2 = new Label("Mail");
-        label2.setAlignment((int) LEFT_ALIGNMENT);
+        label2.setAlignment(Label.LEFT);
         add(label2);
         add(MailId);
-        MailId.getText();
-
+        
+        
+        // for password
         label3 = new Label("password");
-        label3.setAlignment((int) LEFT_ALIGNMENT);
+        label3.setAlignment(Label.LEFT);
         add(label3);
         add(Password);
-        Password.getText();
-
-
+        
+        
+        // for mobile
+        label4 = new Label("Mobie No ");
+        label4.setAlignment(Label.LEFT);
+        add(label4);
+        add(mobileNo);
+        
+        
+        // adding button
         add(button);
 
         button.addActionListener( this);
@@ -50,13 +61,15 @@ public class SignUpForm extends Frame implements  ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
-        System.out.println("Name  : " +Name.getText());
 
+        System.out.println("Name  : " +Name.getText());
         System.out.println("Mail Id   : " +MailId.getText());
         System.out.println("Password   : " +Password.getText());
+        System.out.println("Mobile No  : " +mobileNo.getText());
        
     }
     public static void main(String args []){
         SignUpForm sf = new SignUpForm();
     }
 }
+
